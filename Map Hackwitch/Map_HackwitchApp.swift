517c57295c257da4de79f149216b117a17e6e82b
rohan patel <rpatel5@bsd220.org>
2025-02-03 09:38:23 -0600

    Initial Commit

---
+++ Map Hackwitch/Map_HackwitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwitchApp.swift
+//  Map Hackwitch
+//
+//  Created by Rohan Patel on 2/3/25.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Map HackwitchTests/Map_HackwitchTests.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwitchTests.swift
+//  Map HackwitchTests
+//
+//  Created by Rohan Patel on 2/3/25.
+//
+
+import Testing
+@testable import Map_Hackwitch
+
+struct Map_HackwitchTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
